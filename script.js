38ef8dfd996c45c977f69828263a50fa664e5cd7
Berk Erdem <127400459+berdem98@users.noreply.github.com>
2024-02-26 15:30:36 +0300

    Portfolio
    Portfolio page with HTML JS CSS

---
+++ script.js
@@ -0,0 +1,36 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop-150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id')
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+            })
+        }
+    })
+}
+
+menuIcon.onclick=() =>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+var modeIcon=document.getElementById("mode-icon");
+modeIcon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        modeIcon.src ="images/sun.png";
+    }else{
+        modeIcon.src="images/moon.png";
+    }
+}
